ee02da124bad4a0a10e17c2f38d6197abd1b2014
jiafengwu0301 <jack.wu1224@gmail.com>
2016-10-06 16:40:28 -0600

    done

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+import datetime
+from django.utils import timezone
 
 # Create your models here.
 @python_2_unicode_compatible
@@ -8,6 +10,9 @@ class Question(models.Model):
 	pub_date = models.DateTimeField('date published')
 	def __str__(self):
 		return self.question_text
+	def was_published_recently(self):
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 @python_2_unicode_compatible
 class Choice(models.Model):
